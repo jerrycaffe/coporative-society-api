eb52d5d5088d87502e1005f87f6cf045add01c4b
Habay <biodun.banjoko@yahoo.com>
2023-04-16 08:19:41 +0100

    pending

--- src/main/java/com/cooperativeapi/auth/AuthenticationRequest.java
+++ src/main/java/com/cooperativeapi/auth/AuthenticationRequest.java
@@ -1,11 +1,11 @@
-package com.cooperativeapi.auth;
-
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class AuthenticationRequest {
-
-    private String email;
-    String password;
-}
+//package com.cooperativeapi.auth;
+//
+//@Data
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class AuthenticationRequest {
+//
+//    private String email;
+//    String password;
+//}
